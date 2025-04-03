3b4d24c190409f44c6b4321ae90a67c609b1fc61
Baptiste Parmantier <baptiste.parmantier.pro@gmail.com>
2025-04-03 20:22:20 +0200

    feat: enhance landing page

--- astro.config.mjs
+++ astro.config.mjs
@@ -60,6 +60,6 @@ export default defineConfig({
   },
 
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   }
 });
--- src/lib/components/ui/background-animation.tsx
+++ src/lib/components/ui/background-animation.tsx
@@ -45,7 +45,7 @@ export function BackgroundAnimation() {
   return (
     <div className="bg absolute inset-0 -z-10 transform-gpu blur-3xl overflow-hidden" aria-hidden="true">
       <div
-        className="aspect-[1.7] h-full w-full bg-gradient-to-r from-primary to-white/10 lg:opacity-20 xs:opacity-50"
+        className="aspect-[1.7] h-full w-full bg-gradient-to-r from-primary to-white/10 lg:opacity-20 lg:dark:opacity-40 xs:opacity-50"
         style={{
           clipPath: `polygon(${poly})`,
           transition: 'clip-path 3s'
