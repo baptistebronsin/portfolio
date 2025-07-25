24d6f985b2afbe09ebd9fa4fb71bbbf276d48115
Baptiste Bronsin <baptiste.bronsin@outlook.com>
2025-07-25 07:48:42 +0200

    add alemca experience

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,12 +1,13 @@
 
 export default new Map([
+["src/content/articles/a-mail-server-in-a-kubernetes-cluster.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Fa-mail-server-in-a-kubernetes-cluster.mdx&astroContentModuleFlag=true")],
+["src/content/articles/how-to-avoid-too-many-a-and-aaaa-dns-records-in-your-dns-zone-file.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Fhow-to-avoid-too-many-a-and-aaaa-dns-records-in-your-dns-zone-file.mdx&astroContentModuleFlag=true")],
+["src/content/articles/how-i-configured-my-own-home-server.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Fhow-i-configured-my-own-home-server.mdx&astroContentModuleFlag=true")],
+["src/content/articles/welcome.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Fwelcome.mdx&astroContentModuleFlag=true")],
 ["src/content/projects/beep.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fbeep.mdx&astroContentModuleFlag=true")],
 ["src/content/projects/operator-price.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Foperator-price.mdx&astroContentModuleFlag=true")],
 ["src/content/projects/plannify.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fplannify.mdx&astroContentModuleFlag=true")],
 ["src/content/projects/polyclount.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fpolyclount.mdx&astroContentModuleFlag=true")],
 ["src/content/projects/portfolio.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fportfolio.mdx&astroContentModuleFlag=true")],
-["src/content/articles/a-mail-server-in-a-kubernetes-cluster.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Fa-mail-server-in-a-kubernetes-cluster.mdx&astroContentModuleFlag=true")],
-["src/content/articles/how-i-configured-my-own-home-server.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Fhow-i-configured-my-own-home-server.mdx&astroContentModuleFlag=true")],
-["src/content/articles/how-to-avoid-too-many-a-and-aaaa-dns-records-in-your-dns-zone-file.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Fhow-to-avoid-too-many-a-and-aaaa-dns-records-in-your-dns-zone-file.mdx&astroContentModuleFlag=true")],
-["src/content/articles/welcome.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Fwelcome.mdx&astroContentModuleFlag=true")]]);
+["src/content/projects/postgres-backup.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fpostgres-backup.mdx&astroContentModuleFlag=true")]]);
 		
